5d9f8ee2066f13b80cb54ff29cadc22a68c96b59
Ajantha <ajanthi99720@gmail.com>
2023-05-01 17:05:30 +0530

    1st May

---
+++ backend/Onboarding/src/main/java/com/ajucodes/Onboarding/Repo/MemberRepo.java
@@ -0,0 +1,20 @@
+package com.ajucodes.Onboarding.Repo;
+
+
+import com.ajucodes.Onboarding.Entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@EnableJpaRepositories
+@Repository
+
+public interface MemberRepo extends JpaRepository<Member, Integer> {
+
+    Optional<Member> findOneByEmailAndPassword(String email, String encodedPassword);
+    Member findByEmail(String email);
+
+
+}
